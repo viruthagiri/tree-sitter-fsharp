69839b146a038a6861b93ee6702811ae6d67c774
Viruthagiri Thirumavalavan <giridharanbtech@gmail.com>
2026-03-01 04:01:32 +0530

    go bindings - bug fixes

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,7 +3,7 @@ package tree_sitter_fsharp_test
 import (
 	"testing"
 
-	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter "github.com/github.com/tree-sitter/go-tree-sitter"
 	tree_sitter_fsharp "github.com/tree-sitter/tree-sitter-fsharp/bindings/go"
 )
 
@@ -11,12 +11,5 @@ func TestCanLoadGrammar(t *testing.T) {
 	language := tree_sitter.NewLanguage(tree_sitter_fsharp.Language())
 	if language == nil {
 		t.Errorf("Error loading Fsharp grammar")
-	}
-}
-
-func TestCanLoadSignatureGrammar(t *testing.T) {
-	language := tree_sitter.NewLanguage(tree_sitter_fsharp.LanguageSignature())
-	if language == nil {
-		t.Errorf("Error loading Fsharp signature grammar")
 	}
 }
